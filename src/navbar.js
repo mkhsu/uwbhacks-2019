e325dd50154c60fe925eabd9574221306d3c5f1c
Will Nelson <will@pleb.xyz>
2019-04-19 21:43:21 -0700

    change title

--- src/navbar.js
+++ src/navbar.js
@@ -4,7 +4,7 @@ const NavBar = props => (
     <header className="NavBar">
         <nav className="NavBar__navigation">
             <div></div>
-            <div className="NavBar__logo"><a href="/">King County Income and Tuition Map</a></div>
+            <div className="NavBar__logo"><a href="/">King County Income Map</a></div>
             <div className="NavBar__navigation-items">
                 <ul>
                     <li><a href="/">A Project by Team BigDataCache</a></li>
